dc28c63055ca4b7fc3c82a0a4bad32634b9f3de9
Hamid <hamid.mmr@gmail.com>
2016-04-30 21:36:08 -0400

    some explanatory comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##These functions do exactly what they have to do as decribed in the assignment :)
+
 ## Write a short comment describing this function
+##this functions creates an object that can cache the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
@@ -19,6 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+##this function checks if the inverse exists, if so it returns the inverse, otherwise it calculates the inverse and then returns that
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
